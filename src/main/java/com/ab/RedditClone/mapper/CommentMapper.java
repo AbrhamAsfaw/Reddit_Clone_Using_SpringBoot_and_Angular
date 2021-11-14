a9e8fc07bd1f4148982132115016a7d7eec75a12
AbrhamAsfaw <abrhamasfaw2018@gmail.com>
2021-11-14 16:05:39 +0300

    Finished Implementing Comments API

---
+++ src/main/java/com/ab/RedditClone/mapper/CommentMapper.java
@@ -0,0 +1,22 @@
+package com.ab.RedditClone.mapper;
+
+import com.ab.RedditClone.dto.CommentsDto;
+import com.ab.RedditClone.model.Comment;
+import com.ab.RedditClone.model.Post;
+import com.ab.RedditClone.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface CommentMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "text", source = "commentsDto.text")
+    @Mapping(target = "createdDate", expression = "java(java.time.Instant.now())")
+    @Mapping(target = "post", source = "post")
+    @Mapping(target = "user", source = "user")
+    Comment map(CommentsDto commentsDto, Post post, User user);
+
+    @Mapping(target = "postId", expression = "java(comment.getPost().getPostId())")
+    @Mapping(target = "userName", expression = "java(comment.getUser().getUsername())")
+    CommentsDto mapToDto(Comment comment);
+}
